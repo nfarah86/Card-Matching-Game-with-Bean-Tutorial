43b7af09eb1befef9d3dacf78e92337cfb449a62
Nadine <nfarah86@gmail.com>
2015-12-28 16:59:53 -0800

    edited: added PlayingCardDeck file

---
+++ CardMatchingGameWithBean/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  CardMatchingGameWithBean
+//
+//  Created by Nadine Hachouche on 12/28/15.
+//  Copyright © 2015 nadine farah. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
